211465cc8bc4e8a78fedaa7e2dede41728376c2f
Vinícius Brait <lorimiervb@gmail.com>
2025-09-30 13:29:04 +0000

    feat: first version of implementations

---
+++ log_analyzer_par.c
@@ -0,0 +1,12 @@
+/*
+    Guilherme Teodoro de Oliveira RA: 10425362
+    Luís Henrique Ribeiro Fernandes RA: 10420079
+    Vinícius Brait Lorimier - 10420046
+*/
+#include <stdio.h>
+
+int main(){
+    printf("versão paralela");
+
+    return 0;
+}
---
+++ log_analyzer_seq.c
@@ -0,0 +1,13 @@
+/*
+    Guilherme Teodoro de Oliveira RA: 10425362
+    Luís Henrique Ribeiro Fernandes RA: 10420079
+    Vinícius Brait Lorimier - 10420046
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("versão sequencial");
+
+    return 0;
+}
